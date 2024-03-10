aa95597e9f09fee037d34a5996e4b77e639a4b9d
Osama Salih <osamasalih.ca@gmail.com>
2024-03-10 07:49:55 +0300

    create card order

--- services/orderService.js
+++ services/orderService.js
@@ -9,6 +9,7 @@ const Category = require('../models/categoryModel');
 const Cart = require('../models/cartModel');
 const Restaurant = require('../models/restaurantModel');
 const Item = require('../models/itemsModel');
+const User = require('../models/userModel');
 const Order = require('../models/orderModel');
 
 const updateItemSales = async (cartItems, req) => {
@@ -67,8 +68,10 @@ const checkoutHelper = async (req, cart, totalOrderPrice) =>
     cancel_url: `${req.protocol}://${req.get('host')}/carts`,
     client_reference_id: cart._id.toString(),
     customer_email: req.user.email,
-    metadata: req.body.deliveryAddress,
-    restaurant: req.restaurant,
+    metadata: {
+      deliveryAddress: JSON.stringify(req.body.deliveryAddress),
+      restaurant: req.restaurant,
+    },
   });
 
 // @desc   Create cash order
@@ -165,9 +168,9 @@ exports.checkoutSession = asyncHandler(async (req, res) => {
   // 2) Get item from cart item array
   const item = await checkValueExists(Item, cart.cartItems[0].item.toString());
   // 3) Get category based on item ID that cames from cartItems to get his restaurant document
-  const restaurant = await Category.findById(item.category.toString()).populate(
-    'restaurant',
-  );
+  const { restaurant } = await Category.findById(
+    item.category.toString(),
+  ).populate('restaurant');
   req.restaurant = restaurant._id.toString();
 
   // 4) Calc total order price + restaurant deliveryPrice + taxePrice
@@ -180,17 +183,43 @@ exports.checkoutSession = asyncHandler(async (req, res) => {
   });
 });
 
-// exports.createCardOrder = async session => {
-//   const totalOrderPrice = session.amount_total;
-//   const cartId = session.client_reference_id;
-//   const deliveryAddress = session.metadata;
+// @desc   Create cart order
+const createCardOrder = (session) =>
+  asyncHandler(async (req, res) => {
+    // Get data from session
+    const totalOrderPrice = session.amount_total;
+    const cartId = session.client_reference_id;
+    const { deliveryAddress } = session.metadata;
+    const { restaurant } = session.metadata;
 
-//   const user = await User.findOne({email: session.customer_email});
-//   const cart = await Cart.findById(cartId);
+    const user = await User.findOne({ email: session.customer_email });
+    const cart = await Cart.findById(cartId);
 
-// });
+    // Create cart order
+    const order = await Order.create({
+      user: user._id,
+      cartItems: cart.cartItems,
+      deliveryAddress,
+      restaurant,
+      totalOrderPrice,
+      paymentMethodPrice: 'cart',
+      isPaid: true,
+      PaidAt: Date.now(),
+    });
+    // If order created Update items from cartItems array incerment item sold and decrement item quantity
+    if (order) {
+      updateItemSales(cart.cartItems, req);
+    }
 
-exports.checkout = asyncHandler((req, res, next) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        order,
+      },
+    });
+  });
+
+exports.checkout = asyncHandler(async (req, res) => {
   const sig = req.headers['stripe-signature'];
 
   let event;
@@ -206,6 +235,6 @@ exports.checkout = asyncHandler((req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed') {
-    // createCardOrder(event.data.object);
+    await createCardOrder(event.data.object);
   }
 });
